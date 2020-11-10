b136eed215021fb71e119cf1e10f9b01a4150df5
mrodavell <jomar.llevado@ustp.edu.ph>
2020-11-10 09:44:54 +0800

    Final App

---
+++ precache-manifest.d3f1f360675c1a095fe324d2d6534aa5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "371071b9625af44a6d7872823eee9ac6",
+    "url": "/prc/index.html"
+  },
+  {
+    "revision": "3dd930ec443808e71298",
+    "url": "/prc/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b2dbbdb2887dabc9047d",
+    "url": "/prc/static/css/main.21790f32.chunk.css"
+  },
+  {
+    "revision": "3dd930ec443808e71298",
+    "url": "/prc/static/js/2.3dea6c75.chunk.js"
+  },
+  {
+    "revision": "9a6c3b87f12319bb042df84b89e51ac2",
+    "url": "/prc/static/js/2.3dea6c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2dbbdb2887dabc9047d",
+    "url": "/prc/static/js/main.6c80022b.chunk.js"
+  },
+  {
+    "revision": "cca2613a087b1c7b1051",
+    "url": "/prc/static/js/runtime-main.8b177a24.js"
+  },
+  {
+    "revision": "d0ff5c3ae4b15b2ceb45bfad72e9e9ab",
+    "url": "/prc/static/media/app.d0ff5c3a.apk"
+  },
+  {
+    "revision": "bdddb4d4999ce1cb8ce56e4e0bf50b83",
+    "url": "/prc/static/media/bg.bdddb4d4.jpg"
+  },
+  {
+    "revision": "a06f740351804e09a08694f63d947cdb",
+    "url": "/prc/static/media/icon.a06f7403.png"
+  },
+  {
+    "revision": "0dad67f1333e8b775dad390ccf2c6b79",
+    "url": "/prc/static/media/logo.0dad67f1.png"
+  },
+  {
+    "revision": "a6cbd833e4f2ce6fd49fafeb67000fe4",
+    "url": "/prc/static/media/prc.a6cbd833.png"
+  },
+  {
+    "revision": "58b23abfc04a2b0396339e4735460eb2",
+    "url": "/prc/static/media/ustp.58b23abf.png"
+  }
+]);
